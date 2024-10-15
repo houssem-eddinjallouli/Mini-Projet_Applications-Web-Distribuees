9b733f6307d18d38b227f24b58ca738351a9b6c0
Bacem Jaza <94307612+BacemJaza@users.noreply.github.com>
2024-10-15 17:24:27 +0100

    ApiGateway and eurika configuration

--- microservice-inscriptions/src/main/java/tn/espritclubs/microservice_inscriptions/MicroserviceInscriptionsApplication.java
+++ microservice-inscriptions/src/main/java/tn/espritclubs/microservice_inscriptions/MicroserviceInscriptionsApplication.java
@@ -2,12 +2,17 @@ package tn.espritclubs.microservice_inscriptions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceInscriptionsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MicroserviceInscriptionsApplication.class, args);
+
 	}
+
 
 }
