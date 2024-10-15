1dce08263308afc866a541180d8625b0aba8f0ad
houssem-eddinjallouli <houssem-eddin.jallouli@esprit.tn>
2024-10-15 16:07:48 +0100

    eureka server

---
+++ eureka/src/test/java/tn/_sae7/descovery/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tn._sae7.descovery.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
